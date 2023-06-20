e7e8ae059ae67979dbd18dd997654d245144f045
tajmobile1 <114266082+tajmobile1@users.noreply.github.com>
2023-06-20 21:23:57 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ load_dotenv()
 API_ID = int(os.getenv("API_ID", "9738903"))
 API_HASH = os.environ.get("API_HASH", "d05599b2b23fd03e208ca54a2ff93445")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "5757877293:AAFHlhjUuZImCH2f_5GBVpx6f6igbsdtNEQ")
-ADMINS = [int(i.strip()) for i in os.environ.get("1030188110").split("1030188110")] if os.environ.get("ADMINS") else []
+ADMINS = [int(i.strip()) for i in os.environ.get("ADMINS").split("1030188110")] if os.environ.get("ADMINS") else []
 ADMIN = ADMINS
 DATABASE_NAME = os.environ.get("DATABASE_NAME", "talink")
 DATABASE_URL = os.getenv("DATABASE_URL", "mongodb+srv://converter:l8ZNNpb4Cy1eJvbi@cluster0.gavigeg.mongodb.net/?retryWrites=true&w=majority") 
